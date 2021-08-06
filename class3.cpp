e228e4e67e6b480184b9d8ac6d7218b35d2fc27e
stepanyan18 <87181866+stepanyan18@users.noreply.github.com>
2021-08-06 13:48:30 +0400

    Add files via upload

---
+++ class3.cpp
@@ -0,0 +1,236 @@
+#include<iostream>
+using namespace std;
+
+class data{
+  public:
+	string name[20];
+	string surname[20];
+	int age[20];
+	int year[20];
+	int month[20];
+	int day[20];
+	string helives[20];
+	long int ponenumbers[20];
+};
+int main(){
+	string a,b;
+
+data data1;
+cout<<"name_-_-_-------------";
+	cin>>a;
+	cout<<"surname_-_-_----------";
+	cin>>b;
+
+data1.name[0] = "Artur";
+data1.surname[0] = "Hayrapetyan";
+data1.age[0] = 20;
+data1.year[0] = 2001;
+data1.ponenumbers[0] = +37497379791;
+data1.month[0] = 3; 
+data1.day[0] = 3;
+data1.helives[0]= "XARABAX";
+
+
+	data1.name[1] = "Davit";
+	data1.surname[1] = "Chaloyan";
+	data1.age[1] = 21;
+	data1.year[1] = 2000;
+	data1.ponenumbers[1] = +37433899833;
+	data1.month[1] = 8; 
+	data1.day[1] = 14;
+	data1.helives[1]= "EREVAN";
+
+
+data1.name[2] = "Armen";
+data1.surname[2] = "Margaryan";
+data1.age[2] = 20;
+data1.year[2] = 2001;
+data1.ponenumbers[2] = +37498730623;
+data1.month[2] = 10; 
+data1.day[2] = 14;
+data1.helives[2]= "KIROVAKAN";
+
+
+	data1.name[3] = "Tsolak";
+	data1.surname[3] = "Musikyan";
+	data1.age[3] = 21;
+	data1.year[3] = 2000;
+	data1.ponenumbers[3] = +37498848081;
+	data1.month[3] = 7; 
+	data1.day[3] = 31;
+	data1.helives[3]= "ARARAT";
+
+
+data1.name[4] = "Arsen";
+data1.surname[4] = "Margaryan";
+data1.age[4] = 21;
+data1.year[4] = 2000;
+data1.ponenumbers[4] = +37493644944;
+data1.month[4] = 8; 
+data1.day[4] = 29;
+data1.helives[4]= "EXVARD";
+
+
+	data1.name[5] = "Nver";
+	data1.surname[5] = "Postolakyan";
+	data1.age[5] = 21;
+	data1.year[5] = 2000;
+	data1.ponenumbers[5] = +37498146088;
+	data1.month[5] = 8; 
+	data1.day[5] = 12;
+	data1.helives[5]= "KIROVAKAN";
+
+
+data1.name[6] = "Narek";
+data1.surname[6] = "Stepanyan";
+data1.age[6] = 19;
+data1.year[6] = 2002;
+data1.ponenumbers[6] = +37477263313;
+data1.month[6] = 5; 
+data1.day[6] = 18;
+data1.helives[6]= "ARARAT";
+
+
+	data1.name[7] = "Gevorg";
+	data1.surname[7] = "Karapetyan";
+	data1.age[7] = 21;
+	data1.year[7] = 2000;
+	data1.ponenumbers[7] = +37498355677;
+	data1.month[7] = 1; 
+	data1.day[7] = 26;
+	data1.helives[7]= "ARENI";
+
+
+data1.name[8] = "Erik";
+data1.surname[8] = "Sargsyan";
+data1.age[8] = 20;
+data1.year[8] = 2001;
+data1.ponenumbers[8] = +37498300850;
+data1.month[8] = 8; 
+data1.day[8] = 30;
+data1.helives[8]= "XARABAX";
+
+
+	data1.name[9] = "Hayk";
+	data1.surname[9] = "Vardazaryan";
+	data1.age[9] = 20;
+	data1.year[9] = 2001;
+	data1.ponenumbers[9] = +37477204615;
+	data1.month[9] = 5; 
+	data1.day[9] = 20;
+	data1.helives[9]= "MEXRI";
+
+
+data1.name[10] = "Karen";
+data1.surname[10] = "Hakobyan";
+data1.age[10] = 21;
+data1.year[10] = 2000;
+data1.ponenumbers[10] = +37455611405;
+data1.month[10] = 9; 
+data1.day[10] = 2;
+data1.helives[10]= "EREVAN";
+
+
+	data1.name[11] = "Sergey";
+	data1.surname[11] = "Vardanyan";
+	data1.age[11] = 28;
+	data1.year[11] = 1993;
+	data1.ponenumbers[11] = +37477301314;
+	data1.month[11] = 12; 
+	data1.day[11] = 29;
+	data1.helives[11]= "EREVAN";
+
+
+data1.name[12] = "Arthur";
+data1.surname[12] = "Sedrakyan";
+data1.age[12] = 21;
+data1.year[12] = 2000;
+data1.ponenumbers[12] = +37477548837;
+data1.month[12] = 8; 
+data1.day[12] = 24;	
+data1.helives[12]= "EREVAN";
+
+
+	data1.name[13] = "Atom";
+	data1.surname[13] = "Hunanyan";
+	data1.age[13] = 28;
+	data1.year[13] = 1993;
+	data1.ponenumbers[13] = +37477302317;
+	data1.month[13] = 5; 
+	data1.day[13] = 7;
+	data1.helives[13]= "XARABAX";
+
+
+data1.name[14] = "Khachatur";
+data1.surname[14] = "Hokhanyan";
+data1.age[14] = 25;
+data1.year[14] = 1996;
+data1.ponenumbers[14] = +37477132353;
+data1.month[14] = 1; 
+data1.day[14] = 3;
+data1.helives[14]= "EREVAN";
+
+
+	data1.name[15] = "Hayk";
+	data1.surname[15] = "Minasyan";
+	data1.age[15] = 21;
+	data1.year[15] = 2000;
+	data1.ponenumbers[15] = +37477876797;
+	data1.month[15] = 11; 
+	data1.day[15] = 5;
+	data1.helives[15]= "EREVAN";
+
+
+data1.name[16] = "Ashot";
+data1.surname[16] = "Nikoghosyan";
+data1.age[16] = 21;
+data1.year[16] = 2000;
+data1.ponenumbers[16] = +37477049404;
+data1.month[16] = 7; 
+data1.day[16] = 4;
+data1.helives[16]= "ARARAT";
+
+
+	data1.name[17] = "Vahe";
+	data1.surname[17] = "Sedrakyan";
+	data1.age[17] = 22;
+	data1.year[17] = 1999;
+	data1.ponenumbers[17] = +37493339774;
+	data1.month[17] = 4; 
+	data1.day[17] = 2;
+	data1.helives[17]= "EREVAN";
+
+
+data1.name[18] = "Aghvan";
+data1.surname[18] = "Abgaryan";
+data1.age[18] = 23;
+data1.year[18] = 1998;
+data1.ponenumbers[18] = +37495888337;
+data1.month[18] = 7; 
+data1.day[18] = 24;
+data1.helives[18]= "LENNAKAN";
+
+
+	data1.name[19] = "Mamikon";
+	data1.surname[19] = "Tsarukyan";
+	data1.age[19] = 28;
+	data1.year[19] = 1993;
+	data1.ponenumbers[19] = 37494230893;
+	data1.month[19] = 8; 
+	data1.day[19] = 23;
+	data1.helives[19]= "ARGAVAND";
+	
+for(int i=0; i<=19;i++){	
+	cout<<data1.name[i]<<" "<<data1.surname[i]<<endl;
+	if(a == data1.name[i] && b == data1.surname[i]){
+		cout<<"age_-_-_-------------"
+		<<data1.age[i]<<endl
+		<<"day_-_-_-------------"
+		<<data1.day[i]
+		<<"."<<data1.month[i]
+		<<"."<<data1.year[i]<<"\n"<<"ponenumbers_-_-_------"
+		<<data1.ponenumbers[i]<<"\n"<<"helives_-_-_----------"
+		<<data1.helives[i]<<endl;
+		}	   	
+	}
+}
